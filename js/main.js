bd3470afc42a767a094b1308d7cf36d39469a461
Tanya <tuneyourhead@gmail.com>
2018-05-04 18:33:27 +0300

    contact page

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $( document ).ready(function(){
         console.log(dataform);
         $.ajax({
             type: "POST",
-            url: "https://homecourtadvantage.net/dev/leadform.php",
+            url: "https://homecourtadvantage.net/devsite/leadform.php",
             data: dataform,
             success: function(e) {
                 $("form").find("input[type='submit']").val('submit');
